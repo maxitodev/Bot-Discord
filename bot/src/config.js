6321dcff5265859a183f1ed0d5e9f071891fa5f0
Maxuriel <max.sanchezd13@gmail.com>
2026-02-09 23:00:50 -0600

    feat: add initial bot configuration including tokens, Lavalink nodes, audio settings, embed colors, and emojis.

--- bot/src/config.js
+++ bot/src/config.js
@@ -6,10 +6,10 @@ module.exports = {
     // Lavalink Configuration
     nodes: [
         {
-            host: "lavalinkv4.serenetia.com",
+            host: "localhost",
             port: 443,
-            password: "https://dsc.gg/ajidevserver",
-            secure: true
+            password: "maxitodev",
+            secure: false
         }
     ],
 
